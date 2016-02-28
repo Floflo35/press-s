c74a2cdd987bb2545485d14b319645899691832f
Floflo35 <fchardevel@gmail.com>
2016-02-28 15:45:06 +0100

    bla
    bonjour

---
+++ Assets/Scripts/TriggerGameOver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TriggerGameOver : MonoBehaviour 
+{
+	void OnTriggerEnter (Collider c) 
+	{
+		SceneManager.LoadScene ("GameOver");
+		//if (c.gameObject.name == FPSController) {
+			//Debug.Log ("T'es mort !");
+		//}
+	}
+}
